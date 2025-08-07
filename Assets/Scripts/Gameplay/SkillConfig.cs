a382776342294453240b55a3a93150e1199c42e4
ufukguney <ufukkguney96@gmail.com>
2025-08-07 13:19:43 +0300

    edit skillconfig

--- Assets/Scripts/Gameplay/SkillConfig.cs
+++ Assets/Scripts/Gameplay/SkillConfig.cs
@@ -1,20 +1,9 @@
-public enum SkillType
-{
-    HealthBoost,      
-    AttackBoost,   
-    DefenseBoost,         
-    OpponentAttackDebuff, 
-    OpponentDefenseDebuff, 
-    Shield,               
-    ShieldPenalty       
-}
-
 [System.Serializable]
 public struct SkillConfig
 {
     public SkillType SkillType;
-    public int PrimaryValue;  
-    public int SecondaryValue; 
+    public int PrimaryValue;
+    public int SecondaryValue;
     public string Description;
 
     public SkillConfig(SkillType type, int primary, int secondary = 0, string desc = "")
@@ -24,4 +13,15 @@ public struct SkillConfig
         SecondaryValue = secondary;
         Description = desc;
     }
+}
+
+public enum SkillType
+{
+    HealthBoost,      
+    AttackBoost,   
+    DefenseBoost,         
+    OpponentAttackDebuff, 
+    OpponentDefenseDebuff, 
+    Shield,               
+    ShieldPenalty       
 }
